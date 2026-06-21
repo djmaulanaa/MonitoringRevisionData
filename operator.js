99f28aa67a571b288e4d46093dd0d680553d125e
djmaulanaa <76417917+djmaulanaa@users.noreply.github.com>
2026-06-21 09:37:18 +0800

    Refactor operator.js for improved readability

--- operator.js
+++ operator.js
@@ -187,6 +187,19 @@ function renderCards(data) {
   })
 }
 
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+document.addEventListener('keydown', function (e) {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && e.key === 'I') ||
+    (e.ctrlKey && e.shiftKey && e.key === 'J') ||
+    (e.ctrlKey && e.key === 'U')
+  ) {
+    e.preventDefault();
+  }
+});
+
 // ENTER KEY
 document
   .getElementById('inputSN')
